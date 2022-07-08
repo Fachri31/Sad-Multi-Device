a3bdc39717d4480afdef62e27bd0e2b270530067
fachri <108580447+Fachri31@users.noreply.github.com>
2022-07-08 07:52:27 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-conn.sendHydrated(m.chat, '*『 FazhhBot-Md 』*', `Bot Ini Di Kemabngkan Oleh FachriOfc, FachriOfc Masih Pemula Dan Dia Ingin Tau Tentang Dunia Perbot'an: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
+conn.sendHydrated(m.chat, '*『 FazhhBot-Md 』*', `Bot Ini Di Kembangkan Oleh FachriOfc, FachriOfc Masih Pemula Dan Dia Ingin Tau Tentang Dunia Perbot'an: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
 [`ᴍᴇɴᴜ`, '.m'],
 [`\nɢᴡᴇʜ ꜱᴀᴍɴɢᴇ ᴅᴇɴɢᴀɴ ᴀɴɪᴍᴇʜ`, 'Bilek']
 ], m, {asLocation: true})
